c27c3fd132dadb7e3e14136a5d555715c8fddf0c
jojomoomaw <joseph.moomaw@gmail.com>
2022-03-31 15:03:07 -0500

    try this again

--- script.js
+++ script.js
@@ -27,17 +27,12 @@ window.addEventListener("load", function () {
         getDestinationInfo(document, destinationPlanet.name, destinationPlanet.diameter, destinationPlanet.star, destinationPlanet.distance, destinationPlanet.moon, destinationPlanet.image);
     }).then(function () {
         console.log(listedPlanets);
+        let myPlanet = pickPlanet(listedPlanets);
+        let { name, diameter, star, distance, moons, image: imageUrl } = myPlanet;
+
+        addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
         // addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
- /*   let formData = document.getElementById("formSubmit");
-    formData.addEventListener("click", function (event) {
-        let pilot = document.querySelector("input=[name='pilotName']").value;
-        let copilot = document.querySelector("input=[name='copilotName']").value;
-        let fuelLevel = document.querySelector("input=[name='fuelLevel']").value;
-        let cargoLevel = document.querySelector("input=[name='cargoMass']").value;
-        let list = document.querySelector("faultyItems");
-        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
-    })
-*/
+ 
 });
